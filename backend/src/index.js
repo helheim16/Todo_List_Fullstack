61144c2daff0ed257f4b27068a3306ab340038e4
TalyB19 <beltranaylen2@gmail.com>
2023-11-17 19:41:58 -0300

    Update index.js
    probando

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,6 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.send('HOLA MUNDO'));
-<<<<<<< HEAD
-app.listen(port, () => console.log(`Servidor corriendo en puerto => ${port}!`));
-=======
 app.listen(port, () => console.log(`Servidor corriendo en puerto => ${port}!`));
 // hola
->>>>>>> a9677da649623df1ff2f4514938eddc499d387b8
+// Probando 
