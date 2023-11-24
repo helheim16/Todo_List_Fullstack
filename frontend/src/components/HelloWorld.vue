4abf1562dd168fe02d67d8047a2cc4205603e6c0
TalyB19 <beltranaylen2@gmail.com>
2023-11-24 14:17:17 -0300

    alguito

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,59 +1,201 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container my-4">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <form v-if="mostrarFormulario === 'login'" @submit.prevent="login" action="" class="mt-4 login-form">
+          <h2 class="form-title">Iniciar Sesión</h2>
+          <div class="form-group">
+            <label for="emailLogin">Correo Electrónico:</label>
+            <input type="email" class="form-control" id="emailLogin" v-model="email" />
+          </div>
+          <div class="form-group">
+            <label for="passwordLogin">Contraseña:</label>
+            <input type="password" class="form-control" id="passwordLogin" v-model="password" />
+          </div>
+          <button type="submit" class="btn btn-success mt-4">
+            Iniciar Sesión
+          </button>
+          <p class="mt-2">
+            ¿No tienes una cuenta?
+            <a href="#" @click="cambiarFormulario('registro')">Regístrate</a>
+          </p>
+          <!-- <p class="mt-2">
+            <a href="#" @click="cambiarFormulario('recuperar')">¿Olvidaste tu contraseña?</a>
+          </p> -->
+        </form>
+
+        <form v-else-if="mostrarFormulario === 'registro'" @submit.prevent="register" class="mt-4 register-form">
+          <h2 class="form-title">Registrarse</h2>
+          <div class="form-group">
+            <label for="emailRegister">Correo Electrónico:</label>
+            <input type="email" class="form-control" id="emailRegister" v-model="emailSign" />
+          </div>
+          <div class="form-group">
+            <label for="passwordRegister">Contraseña:</label>
+            <input type="password" class="form-control" id="passwordRegister" v-model="passwordSign" />
+          </div>
+          <div class="form-group">
+            <label for="passwordRepeat">Repetir Contraseña:</label>
+            <input type="password" class="form-control" id="passwordRepeat" v-model="passwordCompare" />
+          </div>
+          <button type="submit" class="btn btn-success mt-4">
+            Registrarse
+          </button>
+          <p class="mt-2">
+            ¿Ya tienes una cuenta?
+            <a href="#" @click="cambiarFormulario('login')">Iniciar Sesión</a>
+          </p>
+        </form>
+
+        <!-- <form v-else @submit.prevent="forgotPassword" class="mt-4 recovery-form">
+          <h2 class="form-title">¿Olvidaste tu Contraseña?</h2>
+          <div class="form-group">
+            <label for="emailForgot">Correo Electrónico:</label>
+            <input type="email" class="form-control" id="emailForgot" v-model="emailForgot" />
+          </div>
+          <button type="submit" class="btn btn-info mt-4 custom-btn">
+            Enviar Correo de Recuperación
+          </button> -->
+          <!-- <p class="mt-2">
+            ¿Recuerdas tu contraseña?
+            <a href="#" @click="cambiarFormulario('login')">Iniciar Sesión</a>
+          </p>
+        </form> -->
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Vue } from "vue-property-decorator";
+import { auth } from "../../firebaseconfig";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail,
+} from "firebase/auth";
 
 @Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+export default class Registro extends Vue {
+  mostrarFormulario: string = "login";
+  email: string = "";
+  emailSign: string = "";
+  password: string = "";
+  passwordSign: string = "";
+  passwordCompare: string = "";
+  emailForgot: string = "";
+
+  login() {
+    signInWithEmailAndPassword(auth, this.email, this.password)
+      .then((credenciales) => {
+        const user = credenciales.user;
+        console.log(user);
+        window.alert("Inicio de sesión Exitoso");
+      })
+      .catch((error) => {
+        window.alert("Fallo el Inicio de Sesión");
+        console.error(error);
+      });
+  }
+
+  register() {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(this.emailSign)) {
+      window.alert("Correo electrónico inválido");
+      return;
+    }
+
+    if (this.passwordSign === this.passwordCompare) {
+      createUserWithEmailAndPassword(auth, this.emailSign, this.passwordSign)
+        .then((credenciales) => {
+          const user = credenciales.user;
+          console.log(user);
+          window.alert("Registro Exitoso");
+
+          this.passwordSign = "";
+          this.passwordCompare = "";
+        })
+        .catch((error) => {
+          window.alert("Fallo el Registro");
+          console.error(error);
+        });
+    } else {
+      window.alert("Las contraseñas no coinciden");
+      this.passwordSign = "";
+      this.passwordCompare = "";
+    }
+  }
+
+  forgotPassword() {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(this.emailForgot)) {
+      window.alert("Correo electrónico inválido");
+      return;
+    }
+
+    // sendPasswordResetEmail(auth, this.emailForgot)
+    //   .then(() => {
+    //     window.alert("Correo de recuperación enviado exitosamente");
+    //     this.emailForgot = "";
+    //     this.mostrarFormulario = "login";
+    //   })
+    //   .catch((error) => {
+    //     window.alert("Error al enviar el correo de recuperación");
+    //     console.error(error);
+    //   });
+  }
+
+  cambiarFormulario(formulario: string) {
+    this.mostrarFormulario = formulario;
+  }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.login-form,
+.register-form,
+.recovery-form {
+  background-color: rgb(231, 243, 227);
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  transition: box-shadow 0.3s ease;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.login-form:hover,
+.register-form:hover,
+.recovery-form:hover {
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.form-title {
+  color: #333;
+  font-size: 1.5em;
+  margin-bottom: 20px;
 }
-a {
-  color: #42b983;
+
+.form-group {
+  margin-bottom: 20px;
+}
+
+label {
+  color: #555;
+}
+
+.btn-success,
+.btn-info {
+  background-color: #4caf50;
+  border: none;
+}
+
+.btn-success:hover,
+.btn-info:hover {
+  background-color: #81c42a;
+}
+
+.btn-success:active,
+.btn-info:active {
+  background-color: #3e8e41;
+}
+
+.custom-btn {
+  color: #fff !important;
 }
 </style>
