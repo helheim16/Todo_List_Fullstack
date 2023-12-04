c9e10c973042a380e030b706c93800424d910f03
Helckya <bouquetyg@gmail.com>
2023-12-04 15:55:24 -0300

    btn crear modificado

--- frontend/src/views/listaCrud.vue
+++ frontend/src/views/listaCrud.vue
@@ -26,7 +26,6 @@
             <div class="input-group rounded buscando">
               <input type="search" class="form-control me-sm-2" placeholder="Search" aria-label="Search"
                 aria-describedby="search-addon" />
-<<<<<<< HEAD
                 <button class="btn btn-info my-2 my-sm-0" type="submit">Buscar</button>
                 <button type="button" class="ms-2 btn btn-success" @click="cambiarFormulario()">{{mostrarFormulario ? "Ocultar" : "Crear"}}</button>
                 
@@ -35,16 +34,6 @@
               <br>
               <br>
               <div  class="d-grid gap-2">
-=======
-                <button class="btn " type="submit"></button>
-                <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
-            </div>
-
-            <br>
-            <br>
-            <div  class="d-grid gap-2">
-              <button type="button" class="btn btn-outline-primary" @click="cambiarFormulario()">{{mostrarFormulario ? "Ocultar" : "Crear"}}</button>
->>>>>>> af4f083ec0ae56ccccd12e9655678f19a515dd00
             </div>
             
            
