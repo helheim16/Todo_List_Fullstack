9cbf769ac03f61d021774ef42f591b4e17e46518
TalyB19 <beltranaylen2@gmail.com>
2023-12-05 20:38:45 -0300

    mejor lista

--- frontend/src/components/Tarea.vue
+++ frontend/src/components/Tarea.vue
@@ -1,22 +1,25 @@
 <template>
     <a href="#" class="list-group-item list-group-item-action d-flex bd-highlight">
         <details class="list-group-item d-flex bd-highlight">
-            <summary class="list-group-item d-flex text-start">
-                <div class=" flex-fill bd-highlight align-middle">
+            <summary class="list-group-item d-flex text-start align-items-center justify-content-between">
+                <div class=" flex-d bd-highlight ">
                     <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckChecked"
                         v-model="tarea.completed">
                     <label class="form-check-label" for="flexCheckChecked">{{ tarea.completed ? 'Completa' : 'Pendiente'
                     }}</label>
                 </div>
-                <p class="flex-fill bd-highlight">{{ tarea.title }}</p>
-                <a id="borrar" class="mx-2" >
+                <div class="flex-d bd-highlight">
+                    <p  class="form-check" >{{ tarea.title }}</p>
+                </div>
+         
+                <a id="borrar" class="mx-1" >
                     <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 448 512">
                         <path fill="#e04950"
                             d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 
                             0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
                     </svg>
                 </a>
-                <a id="editar"  class="mx-2">
+                <a id="editar"  class="mx-1">
                     <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 512 512">
                         <path fill="#3df385"
                             d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9
